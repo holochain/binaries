7b3f26f4945f8208b45a16ef47a0a244998b768e
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-17 12:18:05 +0100

    Update Holochain version

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     };
 
     holochain = {
-      url = "github:holochain/holochain/holochain-0.4.0-dev.12";
+      url = "github:holochain/holochain/holochain-0.4.0-dev.13";
       flake = false;
     };
 
