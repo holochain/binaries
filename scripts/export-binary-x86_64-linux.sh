8d11c0bf2b7a569d7688730ef59a6fd933a10c71
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-24 21:23:22 +0100

    Fix export script and print deps on CI

--- scripts/export-binary-x86_64-linux.sh
+++ scripts/export-binary-x86_64-linux.sh
@@ -2,5 +2,7 @@
 
 name=$1
 out=$2
-cp "./result/bin/$name" "$name"
+mkdir -p "$out"
+cp "./result/bin/$name" "$out/$name-x86_64-linux"
+chmod 777 "$out/$name-x86_64-linux"
 patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --set-rpath /lib/x86_64-linux-gnu "$out/$name-x86_64-linux"
