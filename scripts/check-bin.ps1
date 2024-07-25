5ec69656d7e963b487805de24b84ddf5d7331bda
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-25 20:13:41 +0100

    [no ci] Fix arch name on windows

--- scripts/check-bin.ps1
+++ scripts/check-bin.ps1
@@ -5,6 +5,6 @@ param(
 
 Start-Process gh -ArgumentList "release download $tag --pattern $name-x86_64-windows.exe --repo holochain/holochain"
 
-$proc = Start-Process "$name-x86-64-windows.exe" -ArgumentList "--version" -PassThru -Wait
+$proc = Start-Process "$name-x86_64-windows.exe" -ArgumentList "--version" -PassThru -Wait
 $exit_code = $proc.ExitCode
 echo "$name-result=[$exit_code]" >> $env:GITHUB_OUTPUT
