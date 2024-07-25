6ab05752eaa2a124e02e33c580823594f5901c58
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-25 20:12:24 +0100

    Check windows

---
+++ scripts/check-bin.ps1
@@ -0,0 +1,10 @@
+param(
+    [String]$tag,
+    [String]$name
+    )
+
+Start-Process gh -ArgumentList "release download $tag --pattern $name-x86_64-windows.exe --repo holochain/holochain"
+
+$proc = Start-Process "$name-x86-64-windows.exe" -ArgumentList "--version" -PassThru -Wait
+$exit_code = $proc.ExitCode
+echo "$name-result=[$exit_code]" >> $env:GITHUB_OUTPUT
