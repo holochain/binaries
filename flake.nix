b9c145d08ea963f73e92d8a64b546a4f9f74188c
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-08-28 11:39:25 +0100

    Bump weekly

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     };
 
     holochain = {
-      url = "github:holochain/holochain/holochain-0.4.0-dev.20";
+      url = "github:holochain/holochain/holochain-0.4.0-dev.21";
       flake = false;
     };
 
