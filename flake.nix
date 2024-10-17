bdf3aa09704252899b88449d9cf94c71fcfc22b5
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-17 16:07:16 +0100

    chore: update dev release to dev.1

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     };
 
     holochain = {
-      url = "github:holochain/holochain/?ref=holochain-0.5.0-dev.0";
+      url = "github:holochain/holochain/?ref=holochain-0.5.0-dev.1";
       flake = false;
     };
 
