c1fc9fdfce5e4b68937af848ca0253437d189626
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-11-27 14:28:55 +0000

    chore: Bump weekly

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     };
 
     holochain = {
-      url = "github:holochain/holochain/?ref=holochain-0.5.0-dev.6";
+      url = "github:holochain/holochain/?ref=holochain-0.5.0-dev.7";
       flake = false;
     };
 
