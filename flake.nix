3c0ea0d3a7470d939f2d3c691389c676356d77ff
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-11 12:46:17 +0000

    chore: Bump to 0.4.2 (#11)

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     };
 
     holochain = {
-      url = "github:holochain/holochain/?ref=holochain-0.4.1";
+      url = "github:holochain/holochain/?ref=holochain-0.4.2";
       flake = false;
     };
 
