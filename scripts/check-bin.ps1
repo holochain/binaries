672c9940c5889be9b3753e60baf8e106626486f9
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-09-25 10:59:29 +0100

    ci: New workflow for building binaries at 0.5 (#14)
    ci: Start building default libdatachannel based builds on CI without Nix
    
    chore: wip continue upgrading build steps
    
    wip build bootstrap srv
    
    chore: wip more setup
    
    ci: setup matrix
    
    ci: make it do stuff
    
    ci: add go
    
    ci: source version as lair
    
    ci: improve naming
    
    ci: fix
    
    ci: disable go caching, not needed
    
    ci: update check workflow
    
    ci: only push on dispatch
    
    ci: choose publish on dispatch
    
    chore: Tidy and update README
    
    ci: Remove ubuntu-latest-arm, does not exist
    
    ci: Fixy fixy
    
    ci: Fix
    
    ci: Fix

--- scripts/check-bin.ps1
+++ scripts/check-bin.ps1
@@ -3,8 +3,8 @@ param(
     [String]$name
     )
 
-Start-Process gh -ArgumentList "release download $tag --pattern $name-x86_64-windows.exe --repo holochain/holochain" -Wait
+Start-Process gh -ArgumentList "release download $tag --pattern $name-x86_64-pc-windows-msvc.exe --repo holochain/holochain" -Wait
 
-$proc = Start-Process "$name-x86_64-windows.exe" -ArgumentList "--version" -PassThru -Wait
+$proc = Start-Process "$name-x86_64-pc-windows-msvc.exe" -ArgumentList "--version" -PassThru -Wait
 $exit_code = $proc.ExitCode
 echo "$name-result=[$exit_code]" >> $env:GITHUB_OUTPUT
